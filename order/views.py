97c2266934b6db883d520381520130b0472e9db4
dnetochaves <dnetochaves@gmail.com>
2020-12-27 20:46:02 -0300

    Template product

--- order/views.py
+++ order/views.py
@@ -3,15 +3,17 @@ from django.views.generic.list import ListView
 from django.views import View
 from django.http import HttpResponse
 
+
 class Pay(View):
     def get(*args, **kwargs):
         return HttpResponse('Pay')
+
 
 class CloseOrder(View):
     def get(*args, **kwargs):
         return HttpResponse('CloseOrder')
 
+
 class Details(View):
     def get(*args, **kwargs):
         return HttpResponse('Details')
-
--- product/views.py
+++ product/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from django.http import HttpResponse
+from .models import Product
 
 
-class ListProduct(View):
-    def get(*args, **kwargs):
-        return HttpResponse('ListProduct')
+class ListProduct(ListView):
+    model = Product
+    template_name = 'product/list_product.html'
+    context_object_name = 'products'
+    paginate_by = 5
 
 
-class DetailsProduct(View):
-     def get(*args, **kwargs):
-        return HttpResponse('DetailsProduct')
+class DetailsProduct(DetailView):
+     model = Product
+     template_name = 'product/detail_product.html'
+     context_object_name = 'product'
+     slug_url_kwarg = 'slug'
 
 
 class AddCart(View):
